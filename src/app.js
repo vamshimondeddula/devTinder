2ef9aded127044ea4276d51619aa069096023fe0
vamshimondeddula <166008803+vamshimondeddula@users.noreply.github.com>
2025-08-30 09:59:02 +0530

    Add files via upload
    middleware concept

--- src/app.js
+++ src/app.js
@@ -1,20 +1,18 @@
 const express = require ("express");
  const app = express();
 
-app.get("/user",(req,res)=>{
-    res.send({firstName:"Akshay",lastName:"vamshi"});
-})
+app.use
 
-app.post("/user",(req,res)=>{
-    res.send("data successfully saved to the database");
-})
+app.use("/user",(req,res,next)=>{
+    // res.send("hello from the server")
+    console.log("we are using next here")
+    next();
+},
 
-app.delete("/user",(req,res)=>{
-    res.send("deleted");
-})
-
-app.use("/test",(req,res)=>{
+    (req,res)=>{
     res.send("hello from the server")
+    console.log("we are using next here")
+    
 })
  app.listen(3000,()=>
 console.log("connection established")
